7b4a59ff020881833512c293b9b88370e030842f
KariminK <103779342+KariminK@users.noreply.github.com>
2024-07-30 22:37:40 +0200

    added update method

--- models/message.js
+++ models/message.js
@@ -1,24 +1,30 @@
-class Messages {
+class MessageModel {
   #messages;
   #idCounter;
-  constructor(messages) {
+  constructor() {
     this.#messages = [];
-    this.idCounter = 1;
+    this.#idCounter = 1;
   }
-  get() {
-    return this.#messages;
+  get(id) {
+    if (!id) return this.#messages;
+    else return this.#messages.find((message) => message.id === id);
   }
   add({ text, user }) {
     this.#messages.push({
       text,
       user,
       date: new Date(),
-      id: this.idCounter,
+      id: this.#idCounter,
     });
-    this.idCounter++;
+    this.#idCounter++;
   }
   remove(id) {
     this.#messages = this.#messages.filter((message) => message.id !== id);
   }
+  update(id, newText) {
+    this.#messages.forEach((message) => {
+      if (message.id === id) message.text = newText;
+    });
+  }
 }
-module.exports = Messages;
+module.exports = MessageModel;
--- models/message.test.js
+++ models/message.test.js
@@ -10,7 +10,16 @@ describe("Messages model", () => {
       { text: "lorem ipsum dolor sit amet", user: "example2" },
     ]);
   });
-  it("should add message on add() function call", () => {
+  it("should return single message on get() method call with id in argument", () => {
+    const messages = new Messages();
+    messages.add({ text: "lorem ipsum", user: "example1" });
+    messages.add({ text: "lorem ipsum dolor sit amet", user: "example2" });
+    expect(messages.get(1)).toMatchObject({
+      text: "lorem ipsum",
+      user: "example1",
+    });
+  });
+  it("should add message on add() method call", () => {
     const messages = new Messages();
     messages.add({ text: "lorem ipsum", user: "example1" });
     expect(messages.get()).toMatchObject([
@@ -18,7 +27,7 @@ describe("Messages model", () => {
     ]);
     expect(messages.get()[0]).toHaveProperty("date");
   });
-  it("should remove message with given id", () => {
+  it("should remove message with given id on remove() method call", () => {
     const messages = new Messages();
     messages.add({ text: "lorem ipsum", user: "example1" });
     messages.add({ text: "lorem ipsum dolor sit amet", user: "example2" });
@@ -30,5 +39,12 @@ describe("Messages model", () => {
     expect(messages.get()).toMatchObject([
       { text: "lorem ipsum dolor sit amet", user: "example2", id: 2 },
     ]);
+  });
+  it("should update message's text with given id and new text on update() method call", () => {
+    const messages = new Messages();
+    messages.add({ text: "lorem ipsum", user: "example1" });
+    messages.add({ text: "lorem ipsum dolor sit amet", user: "example2" });
+    messages.update(2, "lorem");
+    expect(messages.get(2).text).toBe("lorem");
   });
 });
