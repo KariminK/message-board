9dba10067ad44288a6829ffb8fee8ffe119d00a9
KariminK <103779342+KariminK@users.noreply.github.com>
2024-07-30 23:15:41 +0200

    coupled things

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,9 @@
 const express = require("express");
-const MessageModel = require("../models/message");
 const indexRouter = express.Router();
+const MessageController = require("../controllers/MessageController");
 
 // TO-DO: new route
-const Messages = new MessageModel();
 
-indexRouter.get("/", (req, res) => {
-  res.render("index", { title: "Online Cafe", messages: Messages.get() });
-});
+indexRouter.get("/", MessageController.getMessages);
 
 module.exports = indexRouter;
