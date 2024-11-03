56d185a85e597e51eb03235fbc1d3576be59b0b9
KariminK <103779342+KariminK@users.noreply.github.com>
2024-11-03 18:54:42 +0100

    conn fix

--- data/pool.js
+++ data/pool.js
@@ -5,6 +5,7 @@ const db = new Pool({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
+  ssl: "require",
 });
 
 module.exports = db;
